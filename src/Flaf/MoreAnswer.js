e2fc1f94b2386a5a75b8ba8ed637493a03211962
LevDomasnih <lev.domasnix2013@gmail.com>
2021-02-09 21:52:35 +0500

    create

--- src/Flaf/MoreAnswer.js
+++ src/Flaf/MoreAnswer.js
@@ -1,20 +1,19 @@
 const josIterative = (n, k) => {
-    if (k === 1) return n // если каждый убивает себя то живой последний!
+    if (k === 1) return n
 
-    let people = [] // создаем массив
-    for (let i = 1; i <= n; i++) people.push(i) // от 1 до числа n закидываем в массив
-    let place = 0 // это номер строчки, для удобства
+    let people = []
+    for (let i = 1; i <= n; i++) people.push(i)
+    let place = 0
 
-    while (people.length > 1) { // пока длина больше или равна 2 то выполняем ->
-        console.log(people) // !!!!!
-        for (let skip = 1; skip < k; skip++) people.push(people.shift()); // из начала достаем элементы и кидаем в конец
-        console.log(people) // !!!!!
-        place += 1 // прибавляем место
-        if (people.length === 2) { // если длина массива 2 то ->
+    while (people.length > 1) {
+        console.log(people)
+        for (let skip = 1; skip < k; skip++) people.push(people.shift())
+        console.log(people)
+        place += 1
+        if (people.length === 2) {
             console.log(`${place}: ${people[people.length - 1]} kill ${people.shift()}`)
-            break // прерываем операцию
+            break
         }
-        // иначе ->
         console.log(`${place}: ${people[people.length - (k-1)]} kill ${people.shift()}`)
     }
     console.log(people + " is survivor")
